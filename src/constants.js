c13e4ce8f80a78498459bf5b0df44f6f81619960
samarth-asthana <samarthasthana110@gmail.com>
2023-06-28 15:27:17 +0530

    search working

---
+++ src/constants.js
@@ -0,0 +1,6 @@
+const constants = {
+  BASE_URL: process.env.REACT_APP_BASE_URL,
+  STORAGE_KEY: process.env.REACT_APP_STORAGE_KEY,
+};
+
+export default constants;
---
+++ src/utils.js
@@ -0,0 +1,75 @@
+function getDocumentHeight() {
+  const body = document.body;
+  const html = document.documentElement;
+
+  return Math.max(
+    body.scrollHeight,
+    body.offsetHeight,
+    html.clientHeight,
+    html.scrollHeight,
+    html.offsetHeight
+  );
+}
+
+function getScrollTop() {
+  return window.pageYOffset !== undefined
+    ? window.pageYOffset
+    : (document.documentElement || document.body.parentNode || document.body)
+        .scrollTop;
+}
+
+export function scrollAreaAvailable() {
+  return getScrollTop() < getDocumentHeight() - window.innerHeight;
+}
+
+export function debounce(func, wait, immediate) {
+  var timeout;
+  return function () {
+    var context = this,
+      args = arguments;
+    var later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
+
+export function throttle(fn, threshhold, scope) {
+  threshhold || (threshhold = 250);
+  var last, deferTimer;
+  return function () {
+    var context = scope || this;
+    var now = new Date(),
+      args = arguments;
+    if (last && now < last + threshhold) {
+      clearTimeout(deferTimer);
+      deferTimer = setTimeout(function () {
+        last = now;
+        fn.apply(context, args);
+      }, threshhold);
+    } else {
+      last = now;
+      fn.apply(context, args);
+    }
+  };
+}
+
+export function checkHttpStatus(response) {
+  if (response.status >= 200 && response.status < 300) {
+    return response;
+  } else {
+    throw response;
+  }
+}
+
+export function parseJSON(response) {
+  return response.json();
+}
+
+export function getImageUrl(farm, server, id, secret) {
+  return `https://farm${farm}.staticflickr.com/${server}/${id}_${secret}.jpg`;
+}
