bbd6d9098b3342625ab7f7cee3dc655927cd281e
samarth-asthana <samarthasthana110@gmail.com>
2023-06-28 17:32:40 +0530

    added safe_search

--- src/constants.js
+++ src/constants.js
@@ -1,8 +1,8 @@
 const constants = {
   BASE_URL:
-    "https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=fc095fc36700a13369004746bc67c545&per_page=10&format=json&nojsoncallback=1&safe_search=3",
+    "https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=fc095fc36700a13369004746bc67c545&per_page=10&format=json&nojsoncallback=1&safe_search=1",
   SEARCH_BASE_URL:
-    "https://api.flickr.com/services/rest/?method=flickr.photos.search&format=json&nojsoncallback=1&api_key=fc095fc36700a13369004746bc67c545&per_page=20&safe_search=3",
+    "https://api.flickr.com/services/rest/?method=flickr.photos.search&format=json&nojsoncallback=1&api_key=fc095fc36700a13369004746bc67c545&per_page=20&safe_search=1",
   STORAGE_KEY: "flicker_search_queries",
 };
 
